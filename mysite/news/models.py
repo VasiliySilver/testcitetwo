13cf3c52f520af3ca54b4cba2cc5af924c2754eb
VasiliySilver <s555133@mail.ru>
2020-06-20 21:56:28 +0300

    remove null=True in categories

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -8,7 +8,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено') # обновление новости
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='фото',blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, blank=True, verbose_name='Категория')
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"news_id": self.pk})
