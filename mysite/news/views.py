5ece1c4d3b3bfef0e6adf2940b7eb4c203aca92d
VasiliySilver <s555133@mail.ru>
2020-06-21 02:04:46 +0300

    добавил класс NewByCategory заменяющий метод get_category а так же поменял путь для категорий

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -26,6 +26,20 @@ class HomeNews(ListView):
     def get_queryset(self):
         return News.objects.filter(is_published=True)
 
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    allow_empty = False  # запрещаем показ пустых списков
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+
 
 
 # def index(request):
