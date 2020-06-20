9ce201b240637654da209f9f63bb766dda28be35
VasiliySilver <s555133@mail.ru>
2020-06-20 22:27:09 +0300

    fix error

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -23,7 +23,7 @@ class NewsForm(forms.ModelForm):
     def clean_title(self):
         title = self.cleaned_data('tilte')
         if re.match(r'\d', title):
-            raise ValueError('Название не должно начинаться с цифры')
+            raise ValidationError('Название не должно начинаться с цифры')
         return title
 
 
