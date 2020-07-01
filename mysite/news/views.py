5c02112cbc5330b07325c39cb63caf6fc03cdf1f
VasiliySilver <s555133@mail.ru>
2020-07-01 18:30:37 +0300

    add mixin for title.upper() for categories don't work add LoginRequiredMixin проверяем аутифинцирован ли пользователь или нет если нет ошибка 403 (raise_exception = True)

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import News, Category
 from .forms import NewsForm
+from .utils import MyMixin
 
 
-class HomeNews(ListView):
+class HomeNews(MyMixin, ListView):
     """
     С помощью класса List View заменяем Index
     model - получаем список всех новостей
@@ -17,17 +19,19 @@ class HomeNews(ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
+    mixin_prop = 'так можно использовать mixin'
     # extra_context = {'title': 'Главная'}
     
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(HomeNews, self).get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
+        context['title'] = self.get_upper('Главная страница')
+        context['mixin_prop'] = self.get_prop()
         return context
 
     def get_queryset(self):
         return News.objects.filter(is_published=True)
 
-class NewsByCategory(ListView):
+class NewsByCategory(MyMixin, ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
@@ -35,7 +39,7 @@ class NewsByCategory(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        context['title'] = self.get_upper(Category.objects.get(pk=self.kwargs['category_id']))
         return context
 
     def get_queryset(self):
@@ -49,10 +53,12 @@ class ViewNews(DetailView):
     # template_name = 'news/news_detail.html'
 
 
-class CreateNews(CreateView):
+class CreateNews(LoginRequiredMixin, CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
     # success_url = reverse_lazy('home') # куда редиректить
+    # login_url = '/admin/'
+    raise_exception = True
 
 
 # def index(request):
