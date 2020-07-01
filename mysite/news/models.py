208b821107c88dbfae38eb1870e2ee2accfe6887
VasiliySilver <s555133@mail.ru>
2020-07-01 17:41:30 +0300

    fix models.py

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.urls import reverse
 
+
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Наименование')
-    content = models.TextField(blank=True, verbose_name='Контент') # если blsnk то поле не обязательно к заполнению
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации') # добавление новости
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено') # обновление новости
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='фото',blank=True)
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, blank=True, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    views = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"pk": self.pk})
@@ -19,7 +21,8 @@ class News(models.Model):
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
-        ordering = ['-created_at'] # в каком порядке показывать новость
+        ordering = ['-created_at']
+
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
